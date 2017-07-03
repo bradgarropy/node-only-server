4dddfb8a977ddbf97e2dbfdc6c9e32871daccb37
Brad Garropy <bradgarropy@gmail.com>
2017-07-03 17:09:04 -0500

    Created API endpoints for GET and POST.

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
-var http = require("http");
-var fs = require("fs");
+const http = require("http");
+const qs = require("querystring");
+const fs = require("fs");
 
 
 requestHandler = function(request, response) {
 
+    // GET Method
     if(request.method == "GET") {
 
-        console.log(request.url);
-        console.log(request.method);
-
-        if (request.url == "/favicon.ico") {
-            response.writeHead(404);
-            response.write("<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>");
-            response.end();
-        }
-        else if(request.url == "/") {
-            var weights = fs.readFileSync("weight.json");
+        if(request.url == "/") {
+            let weights = fs.readFileSync("weight.json");
             weights = JSON.parse(weights);
 
-            var data = "";
-            for (var i = 0; i < weights.length; i++) {
-                data += "<li>" + weights[i].date + " - " + weights[i].weight + "</li>";
-            }
+            let data = "";
+            weights.forEach(function(weight) {
+                data += "<tr><td>" + weight.date + "</td><td>" + weight.weight + "</td></tr>";
+            });
 
-            var html =
+            let html =
             `
             <html>
                 <head></head>
 
                 <body>
-                    <ul>
+
+                    <table>
+                        <tr>
+                            <th>Date</th>
+                            <th>Weight</th>
+                        </tr>
                         ${data}
-                    </ul>
+                    </table>
+
                 </body>
             </html>
             `;
 
             response.write(html);
+            response.end();
+        }
+        if(request.url == "/api/weight") {
+            let weights = fs.readFileSync("weight.json");
+
+            response.write(weights);
             response.end();
         }
         else {
@@ -45,11 +51,47 @@ requestHandler = function(request, response) {
             response.end();
         }
     }
-    else if (request.method == "POST") {
-        console.log(request.url);
-        console.log(request.method);
 
-        response.write(request.method + " request to " + request.url);
+    // POST Method
+    else if (request.method == "POST") {
+
+        if(request.url == "/api/weight") {
+            let query = "";
+
+            // read all incoming data
+            request.on('data', function(data) {
+                query += data;
+            });
+
+            // perform actions
+            request.on('end', function() {
+
+                // parse post data
+                let post = qs.parse(query);
+
+                // convert weight to int
+                post.weight = parseInt(post.weight);
+
+                // add new weight
+                let weights = fs.readFileSync("weight.json");
+                weights = JSON.parse(weights);
+                weights.push(post);
+
+                response.write(JSON.stringify(weights));
+                response.end();
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.write("<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>");
+            response.end();
+        }
+    }
+
+    // Unknown Method
+    else {
+        response.writeHead(404);
+        response.write("<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>");
         response.end();
     }
 
