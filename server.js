4068bf080b2aeaa7db4d6a71e661a5059caa82de
Brad Garropy <bradgarropy@gmail.com>
2017-07-04 20:37:20 -0500

    Implementing DELETE method.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const querystring = require("querystring");
 const http        = require("http");
-const url         = require('url');
 const fs          = require("fs");
 
 
@@ -13,14 +12,18 @@ requestHandler = function(request, response) {
     if(request.method == "GET") {
 
         if(request.url == "/") {
+
+            // read weight file
             let weights = fs.readFileSync("weight.json");
             weights = JSON.parse(weights);
 
+            // create html table
             let data = "";
             weights.forEach(function(weight) {
                 data += "<tr><td>" + weight.date + "</td><td>" + weight.weight + "</td></tr>";
             });
 
+            // html template
             let html =
             `
             <html>
@@ -40,12 +43,16 @@ requestHandler = function(request, response) {
             </html>
             `;
 
+            // send response
             response.write(html);
             response.end();
         }
         else if(request.url == "/api/weight") {
+
+            // read weight file
             let weights = fs.readFileSync("weight.json");
 
+            // send response
             response.write(weights);
             response.end();
         }
@@ -76,13 +83,18 @@ requestHandler = function(request, response) {
                 // convert weight to int
                 post.weight = parseInt(post.weight);
 
-                // add new weight
+                // read weight file
                 let weights = fs.readFileSync("weight.json");
                 weights = JSON.parse(weights);
+
+                // add new weight
                 weights.push(post);
+
+                // write weight file
                 weights = JSON.stringify(weights, null, 4);
                 fs.writeFileSync("weight.json", weights);
 
+                // send response
                 response.write(weights);
                 response.end();
             });
@@ -97,16 +109,33 @@ requestHandler = function(request, response) {
     // Delete Method
     else if(request.method == "DELETE") {
 
-        // parse date from uri
-        let u = url.parse(request.url);
-        let pathname = u.pathname.split("/");
-        let date = pathname[pathname.length - 1];
+        // test for /api/weight/:date endpoint
+        let regex = /\/api\/weight\/\d{4}-\d{2}-\d{2}/i;
+        if(regex.test(request.url)) {
 
-        // giving up here
-        // parsing uri's with variable data using regexes is crazy
-        // time to use express
-        if(request.url == "/api/weight/:date") {
-            console.log("Found a date to delete!");
+            // parse date from url
+            let date = request.url.split("/");
+            date = date[date.length - 1];
+
+            // read weight file
+            let weights = fs.readFileSync("weight.json");
+            weights = JSON.parse(weights);
+
+            // find and remove date
+            for(let i = 0; i < weights.length; i++) {
+                if (weights[i].date == date) {
+                    weights.splice(i, 1);
+                    break;
+                }
+            }
+
+            // write weight file
+            weights = JSON.stringify(weights, null, 4);
+            fs.writeFileSync("weight.json", weights);
+
+            // send response
+            response.write(weights);
+            response.end();
         }
         else {
             response.writeHead(404);
@@ -126,8 +155,8 @@ requestHandler = function(request, response) {
 };
 
 
-var port = 3000;
-var server = http.createServer(requestHandler);
+let port = 3000;
+let server = http.createServer(requestHandler);
 server.listen(port, function() {
     console.log("Server listening on port %s.", port);
 });
